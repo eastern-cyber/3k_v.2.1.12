1087fb3975bebb1b0da1365ccb5ba1ad0497914e
katawutp <katawut.paramee@gmail.com.com>
2026-01-19 15:27:26 +0700

    setting MEDIA_URL

--- _core/settings.py
+++ _core/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [ BASE_DIR / "static"]
 
-MEDIA_DIR = 'media/'
+MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
