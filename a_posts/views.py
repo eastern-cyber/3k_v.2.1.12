fde45d9b07d62c428a8c3c4221108d7c40d86842
katawutp <katawut.paramee@gmail.com.com>
2026-01-29 03:40:59 +0700

    form

--- a_posts/views.py
+++ a_posts/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import PostForm
 from .models import Post
 
 def home_view(request):
-    posts = Post.objects.all()
+    posts = Post.objects.order_by('-created_at')
     context = {
         'page': 'Home',
         'posts': posts,
@@ -14,9 +15,12 @@ def home_view(request):
 
 
 def explore_view(request):
+    posts = Post.objects.order_by('-created_at')
     context = {
         'page': 'Explore',
+        'posts': posts,
         'partial': request.htmx,
+
     }
     if request.htmx:
         return render(request, 'a_posts/partials/_explore.html', context)
@@ -24,8 +28,19 @@ def explore_view(request):
 
 
 def upload_view(request):
+    form = PostForm()
+    
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('home')
+    
     context = {
         'page': 'Upload',
+        'form': form,
         'partial': request.htmx,
     }
     if request.htmx:
